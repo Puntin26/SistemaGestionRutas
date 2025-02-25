5da3572ada6bca6e353ba3218969a72a95a4bafb
Puntin26 <alvarezian0912@gmail.com>
2025-02-25 17:44:45 -0400

    Inicio ProyFinal

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.demoproyfinal {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.demoproyfinal to javafx.fxml;
+    exports com.example.demoproyfinal;
+}
